0daaa3900f872989ea882c7be9e757d3ba165720
nuck <97256239+nick4597@users.noreply.github.com>
2025-10-27 19:38:54 +0400

    e

--- mzzziiiurr/src/components/counterbuton.jsx
+++ mzzziiiurr/src/components/counterbuton.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 export default function CounterButton({ title, handleClick }) {
   return (
     <button
@@ -13,3 +15,9 @@ export default function CounterButton({ title, handleClick }) {
     </button>
   );
 }
+const MemorizCounterButton = React.memo(CounterButton);
+export {MemorizCounterButton};
+console.log("CounterButton component loaded"
+); console.log("CounterButton component loaded");
+const a = 10; b = 20; console.log(a + b);
+const x = 5; y = 15; console.log(x * y);
