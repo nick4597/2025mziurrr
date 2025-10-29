e300348af8dadc304516485ccfcaef3fcad3fdad
nuck <97256239+nick4597@users.noreply.github.com>
2025-10-29 19:53:53 +0400

    sasas

--- mzzziiiurr/src/App.jsx
+++ mzzziiiurr/src/App.jsx
@@ -1,26 +1,28 @@
-import { useState } from "react";
+import Player from "./components/Player";
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const projectName = "Clicker-Game";
+
+  let playerRandomNumber = 0;
+
+  const rollDice = () => {
+    const randomNumber = Math.floor(Math.random() * 6) + 1;
+    setPlayer1(randomNumber);
+    setPlayer2(randomNumber);
+  };
 
   return (
-    <>
-      <h1>{projectName}</h1>
-      <h2>Counter: {counter}</h2>
-      <button
-        style={{
-          margin: "5px",
-          padding: "10px 20px",
-          cursor: "pointer",
-          borderRadius: "8px",
-        }}
-        onClick={() => {
-          setCounter(counter + 1);
-        }}
-      >
-        Increase
-      </button>
+    <div
+      style={{
+         textAlign: "center",
+         padding: "20px",
+         margin: 0,
+      }}
+    >
+        <h1>2 Player Dice Game</h1>
 
+      </div>
+        <Player title={"Player 1"} index={3} handleclick={}/>
 
+        <div>VC</div>
+    );
 export default App;
