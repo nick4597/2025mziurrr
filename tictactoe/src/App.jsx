fb0e55d8a03d19aa5480cf16452648dd8ce9916b
nuck <97256239+nick4597@users.noreply.github.com>
2025-12-03 20:05:58 +0400

    sasas

--- tictactoe/src/App.jsx
+++ tictactoe/src/App.jsx
@@ -5,9 +5,32 @@ import Log from "./components/Log";
 
 function App() {
   const [activePlayer, setActivePlayer] = useState("X");
+  const [gameTurns, setGameTurns] = useState([]);
 
   function handleCurrentPlayerChange() {
-    setActivePlayer((prevValue) => (prevValue === "X" ? "O" : "X"));
+  //  setActivePlayer((prevValue) => (prevValue === "X" ? "O" : "X"));
+  
+  setActivePlayer((revValue) => (prevValue === "X" ? "O" ; "X"));
+  setGameTurns((prevGameTurns)) => {}
+    let currentPlayer = "X";
+
+    if (prevGameTurns.length > 0 && prevGameTurns[0] === "X")}
+      currentPlayer = "0";
+  }
+
+  const updatedTurns= {
+    { symbol: currentPlayer, square: { row: rowIndex, col: colldex } },
+    ...prevGameTurns,
+  };
+
+  return updatedTurns;
+ }};
+}
+
+
+
+
+
   }
 
   return (
